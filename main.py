dd64c017ff2faaa784d6fb849fbbf2f55ae58c5b
David Rakovsky <david.rakovsky@centrum.cz>
2022-11-08 11:02:10 +0100

    Pridany dalsi funkce do souboru main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def report(data):
 
 def coins():
     print("Prosim vlozte mince 1. 2, 5, 10, 20, 50")
-    kc1 = int(input("Kolik 1 Kc chcete vlozit?: "))
+    kc1 = int(input("Kolik 1 Kc chcete vlozit?: ")) * 1
     kc2 = int(input("Kolik 2 Kc chcete vlozit?: ")) * 2
     kc5 = int(input("Kolik 5 Kc chcete vlozit?: ")) * 5
     kc10 = int(input("Kolik 10 Kc chcete vlozit?: ")) * 10
@@ -36,18 +36,41 @@ def calculate_change(user_sum_coins, price):
     else:
         print(f"Nevhodili jste dostatek penez, je zapotrebi vlozit jeste {price - user_sum_coins} Kc")
 
+def fill_in_ingredients():
+    return(resources)
+
+def calculate_ingredients(drink_name):
+    if drink_name == "espresso":
+        rest_of_ingredience["water"] = rest_of_ingredience["water"] - MENU["espresso"]["ingredients"]["water"]
+        rest_of_ingredience["milk"] = rest_of_ingredience["milk"] - MENU["espresso"]["ingredients"]["milk"]
+        rest_of_ingredience["coffee"] = rest_of_ingredience["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
+        print(f"Zbyle ingredience: {rest_of_ingredience}")
 
 
 
-# Kod automatu
+
+
+# ==Kod automatu==
+# Volba uzivatele, jaky chce napoj
 user_choice = input("Co byste si dal/a? (espresso/latte/cappuccino): ")
 
-if user_choice == "report":
-    report(resources)
+# Nacitame puvodni mnozstvi ingredienci
+rest_of_ingredience = fill_in_ingredients
 
+# Kontrolni report
+if user_choice == "report":
+    report(rest_of_ingredience)
+
+# Hlavni kod automatu
 if user_choice == "espresso":
     sum = coins()
+    print(f"Cena espressa je {espresso_price}")
+    calculate_change(sum, espresso_price)
 elif user_choice == "latte":
     sum = coins()
+    print(f"Cena latte je {latte_price}")
+    calculate_change(sum, latte_price)
 elif user_choice == "cappuccino":
     sum = coins()
+    print(f"Cena cappuccina je {cappuccino_price}")
+    calculate_change(sum, cappuccino_price)
